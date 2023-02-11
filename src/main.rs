f19654c9d6629f2b89543c01ea63f213e38bd534
clemmsch <clemens.schuetz@protonmail.com>
2023-02-11 15:05:11 +0100

    update

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 mod notes;
-use std::{io, thread::sleep_ms, time::Duration};
+use std::{io, time::Duration};
 use rand::prelude::*;
 
 use crate::notes::{Note, play_note};
@@ -8,9 +8,102 @@ use crate::notes::{Note, play_note};
 
 fn main() {
     loop {
+        println!("Enter a command: ");
+        let mut buffer = String::new();
+        let stdin = io::stdin();
+        while stdin.read_line(&mut buffer).is_ok() {
+            match buffer.as_str().trim_end() {
+                "eq" => { are_equal() }
+                "which" => { which() }
+                "is_note" => { is_note() }
+                "ten_of" => { ten_of() }
+                _ => { println!("Unknown command '{}'.", buffer)}
+            }
+        }    
+    }
+}
+
+pub fn ten_of() {
+    let note = {
+        print!("Which note do you want to listen to for?");
+        let mut buffer = String::new();
+        let stdin = io::stdin();
+        stdin.read_line(&mut buffer).unwrap();
+        let e = Note::try_from(buffer.trim_end());
+        if let Ok(note) = e {
+            note
+        } else {            
+            println!("Invalid note");
+            return;
+        }
+    };
+    for i in 0..10 {
+        println!("Playing {:?}{}", note, i);
+        play_note(note, i);
+        std::thread::sleep(Duration::from_millis(200));
+    }
+
+}
+
+pub fn which() {}
+
+pub fn is_note() {
+    let note = {
+        print!("Which note do you want to listen out for?");
+        let mut buffer = String::new();
+        let stdin = io::stdin();
+        stdin.read_line(&mut buffer).unwrap();
+        let e = Note::try_from(buffer);
+        if let Ok(note) = e {
+            note
+        } else {            
+            println!("Invalid note");
+            return;
+        }
+    };
+
+
+    let mut correct = 0;
+    let mut offness = 0;
+    let mut incorrect = 0;
+    for _ in 0..10 {
         let mut rng = thread_rng();
         let note_a: Note = unsafe { std::mem::transmute((rng.gen_range(0_u32..11_u32))) }; 
-        let octave_a = rng.gen_range(2..8);
+        let octave_a = rng.gen_range(3..8);
+
+        let same = note_a == note;
+
+        play_note(note_a, octave_a);
+        print!("Was this a {:?} (e = equal, n = not equal)", note);
+        let mut buffer = String::new();
+        let stdin = io::stdin();
+        if stdin.read_line(&mut buffer).is_ok() {
+            // Trim end.
+            let trimmed = buffer.trim_end();
+            let answer = trimmed == "e";
+            if answer == same {
+                correct += 1;
+                println!("You were correct: The first note was {:?}{}, the original note was {:?}. Your answer: {}. Correct answer {}.", note_a, octave_a, note, same, answer);
+            } else {
+                incorrect += 1;
+                offness += (note_a as i32 - note as i32).abs() as u32;
+                println!("You were incorrect: The first note was {:?}{}, the original note was {:?}. Your answer: {}. Correct answer {}.", note_a, octave_a, note, same, answer);
+            }
+            buffer.clear();
+        }
+    }
+    println!("Correct: {}; Incorrect {}, avg. offness: {}, percentage correct: {}", correct, incorrect, offness / incorrect, correct / incorrect);
+}
+
+
+pub fn are_equal() {
+    let mut correct = 0;
+    let mut offness = 0;
+    let mut incorrect = 0;
+    for _ in 0..10 {
+        let mut rng = thread_rng();
+        let note_a: Note = unsafe { std::mem::transmute((rng.gen_range(0_u32..11_u32))) }; 
+        let octave_a = rng.gen_range(3..8);
 
         let same: bool = rng.gen();
         let note_b = if same { note_a } else { 
@@ -18,8 +111,8 @@ fn main() {
             if gen == (note_a as u32) { gen += 1 }  
             unsafe { std::mem::transmute(gen) }
         };
-        let mut octave_b = rng.gen_range(2..8);
-        if octave_b == octave_a { octave_b += 1; }
+        let mut octave_b = rng.gen_range(3..8);
+        if octave_b == octave_a {  octave_b += 1; }
 
         play_note(note_a, octave_a);
         std::thread::sleep(Duration::from_millis(500));
@@ -30,15 +123,18 @@ fn main() {
         if stdin.read_line(&mut buffer).is_ok() {
             // Trim end.
             let trimmed = buffer.trim_end();
-            println!("You typed: [{trimmed}]");
             let answer = trimmed == "e";
             if answer == same {
+                correct += 1;
                 println!("You were correct: The first note was {:?}{}, the second note was {:?}{}. Your answer: {}. Correct answer {}.", note_a, octave_a, note_b, octave_b, same, answer);
             } else {
+                incorrect += 1;
+                offness += (note_a as i32 - note_b as i32).abs() as u32;
                 println!("You were incorrect: The first note was {:?}{}, the second note was {:?}{}. Your answer: {}. Correct answer {}.", note_a, octave_a, note_b, octave_b, same, answer);
             }
             buffer.clear();
         }
-        println!("lmao");
     }
+    println!("Correct: {}; Incorrect {}, avg. offness: {}, percentage correct: {}", correct, incorrect, offness / incorrect, correct / incorrect);
+
 }
--- src/notes.rs
+++ src/notes.rs
@@ -42,12 +42,57 @@ impl Note {
     }
 }
 
+impl TryFrom<String> for Note {
+    type Error = ();
+
+    fn try_from(value: String) -> Result<Self, Self::Error> {
+        match value.as_str() {
+            "C" =>  Ok(Self::C),
+            "C#" => Ok(Self::CSharp),
+            "D" =>  Ok(Self::D),
+            "D#" => Ok(Self::DSharp),
+            "E" =>  Ok(Self::E),
+            "F" =>  Ok(Self::F),
+            "F#" => Ok(Self::FSharp),
+            "G" =>  Ok(Self::G),
+            "G#" => Ok(Self::GSharp),
+            "A" =>  Ok(Self::A),
+            "A#" => Ok(Self::ASharp),
+            "B" =>  Ok(Self::B),
+            _ => Err(())
+        }
+    }
+}
+
+impl TryFrom<&str> for Note {
+    type Error = ();
+
+    fn try_from<'a>(value: &'a str) -> Result<Self, Self::Error> {
+        match value {
+            "C" =>  Ok(Self::C),
+            "C#" => Ok(Self::CSharp),
+            "D" =>  Ok(Self::D),
+            "D#" => Ok(Self::DSharp),
+            "E" =>  Ok(Self::E),
+            "F" =>  Ok(Self::F),
+            "F#" => Ok(Self::FSharp),
+            "G" =>  Ok(Self::G),
+            "G#" => Ok(Self::GSharp),
+            "A" =>  Ok(Self::A),
+            "A#" => Ok(Self::ASharp),
+            "B" =>  Ok(Self::B),
+            _ => Err(())
+        }
+    }
+}
+
+
 
 pub fn play_note(note: Note, octave: i32) {
     let (_stream, stream_handle) = OutputStream::try_default().unwrap();
     let sink = Sink::try_new(&stream_handle).unwrap();
 
-    let freq = note.octave(5);
+    let freq = note.octave(octave);
 
     let source = SineWave::new(freq).take_duration(Duration::from_secs_f32(3.0)).amplify(1.0);
     sink.append(source);
